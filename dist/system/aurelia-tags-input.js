6eb7ba7ac09d026d4d068d6ca238d280dfc77181
Tiago Morais <tiago.morais@fonixtelematics.com>
2016-11-24 11:13:53 +0000

    first commit

---
+++ dist/system/aurelia-tags-input.js
@@ -0,0 +1,24 @@
+"use strict";
+
+System.register([], function (_export, _context) {
+  "use strict";
+
+  var AureliaTagsInput;
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  return {
+    setters: [],
+    execute: function () {
+      _export("AureliaTagsInput", AureliaTagsInput = function AureliaTagsInput() {
+        _classCallCheck(this, AureliaTagsInput);
+      });
+
+      _export("AureliaTagsInput", AureliaTagsInput);
+    }
+  };
+});
